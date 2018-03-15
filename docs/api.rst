6b39da0d8b1a09d62c7082048171e45b7ec26649
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-15 15:09:16 -0500

    Initial checkin

---
+++ docs/api.rst
@@ -0,0 +1,8 @@
+===========
+ Reference
+===========
+
+Interfaces
+==========
+
+.. automodule:: nti.app.orgsync_spark.interfaces
---
+++ setup.py
@@ -0,0 +1,82 @@
+import codecs
+from setuptools import setup
+from setuptools import find_packages
+
+entry_points = {
+    'console_scripts': [
+        "nti_orgsync_spark_runner = nti.app.orgsync_spark.scripts.nti_orgsync_spark_runner:main",
+    ],
+    "z3c.autoinclude.plugin": [
+        'target = nti.app',
+    ],
+}
+
+TESTS_REQUIRE = [
+    'nti.app.testing',
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.app.orgsync_spark',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI orgsync spark App Layer",
+    long_description=(
+        _read('README.rst')
+        + '\n\n'
+        + _read("CHANGES.rst")
+    ),
+    license='Apache',
+    keywords='pyramid orgsync spark',
+    classifiers=[
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.app.orgsync_spark",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.app'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'nti.app.spark',
+        'nti.orgsync_spark',
+        'nti.spark',
+        'pyramid',
+        'six',
+        'zope.cachedescriptors',
+        'zope.component',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.lifecycleevent',
+        'zope.security',
+        'zope.traversing',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
